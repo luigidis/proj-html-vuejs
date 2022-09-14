ef51a9b75147bf44e0046c3f9c3b078b58dde4e9
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-14 16:46:00 +0200

    Add Imgs/Add variables.scss,main.scss /NavStart

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,6 @@ export default {
 </script>
 
 <style lang="scss">
+@import './styles/main.scss';
 
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-        NavBar
+    <div class="bg-nav">
+        <nav class="nav-wrapper container py-2">
+            <div class="nav-logo">
+                <img src="../assets/img/logo.png" alt="Logo Avada Forum">
+            </div>
+            <ul class="nav-list-wrapper">
+                <li class="nav-item">Home</li>
+                <li class="nav-item">Apple</li>
+                <li class="nav-item">Microsoft</li>
+                <li class="nav-item">Android</li>
+                <li class="nav-item">Forums</li>
+                <li class="nav-item">Contact us</li>
+                <li class="nav-item">
+                    <a href="#">Join Us</a>
+                </li>
+                <li class="nav-item">
+                    Search
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -14,7 +32,27 @@ export default {
 @import '../styles/variables.scss';
 
 * {
-    font-family: $main-font-family;
+    
+
+    .bg-nav {    
+        background-color: $easternBlue-color;
+        color: white;
+        font-size: 0.813rem;
+        .nav-wrapper {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            .nav-log {
+                max-width: 237px;
+            }
+            .nav-list-wrapper {
+                display: flex;
+                gap: 30px;
+            }
+                
+        }
+    }
+
 }
 
 </style>
