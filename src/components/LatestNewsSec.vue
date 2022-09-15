ab6cc7f6b60cc73f5be4f56ee44b162a9d359e19
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-15 11:52:23 +0200

    section news in

--- src/components/LatestNewsSec.vue
+++ src/components/LatestNewsSec.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="text-center">
-        ALL THE LATEST NEWS
+    <div class="news_container container">
+        <div class="title_news">
+            <h3>Phasellus Eget Metus</h3>
+            <h2>All the latest news</h2>
+        </div>
+        Componente card triplo
+        Componente card singola
+        Componente card triplo
+        Componente card singolo + triplo in colonna
     </div>
 </template>
 
@@ -11,5 +18,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/variables.scss';
 
+.news_container {
+    padding-top: 115px;
+    .title_news {
+        position: relative;
+        h3 {
+            text-transform: uppercase;
+            font-size: 12px;
+            text-align: center;
+            font-weight: bold;
+            letter-spacing: 5px;
+            padding-bottom: 25px;
+            color: $capeCod-color;
+        }
+        h2 {
+            font-weight: bold;
+            font-size: 42px;
+            letter-spacing: 2%;
+            text-align: center;
+            padding-bottom: 42px;
+        }
+    }
+    .title_news::after {
+        content: "";
+        width: 80px;
+        height: 3px;
+        background-color: salmon;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translate(-50%, -50%)
+    }
+}
 </style>
+       
+        
+    
